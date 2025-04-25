dfa37070b9d0981513b21e21cf48099dae1c1768
newit-duylcq <newit-duylcq@newit.co.jp>
2025-04-25 22:36:47 +0700

    code done

---
+++ app/public/wp-content/themes/zekweb/template-parts/section-overview.php
@@ -0,0 +1,58 @@
+<section class="section-overview">
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-6 col-md-3">
+                <div class="overview-item">
+                    <div class="overview-icon">
+                        <figure>
+                            <img src="<?php bloginfo('template_url'); ?>/assets/images/Medal.png" alt="icon" loading="lazy">
+                        </figure>
+                    </div>
+                    <div class="overview-content text-center">
+                        <h3 class="overview-title">2008</h3>
+                        <p class="overview-text">17 năm kinh nghiệm</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-3">
+                <div class="overview-item">
+                    <div class="overview-icon">
+                        <figure>
+                            <img src="<?php bloginfo('template_url'); ?>/assets/images/Medal.png" alt="icon" loading="lazy">
+                        </figure>
+                    </div>
+                    <div class="overview-content text-center">
+                        <h3 class="overview-title">2008</h3>
+                        <p class="overview-text">17 năm kinh nghiệm</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-3">
+                <div class="overview-item">
+                    <div class="overview-icon">
+                        <figure>
+                            <img src="<?php bloginfo('template_url'); ?>/assets/images/Medal.png" alt="icon" loading="lazy">
+                        </figure>
+                    </div>
+                    <div class="overview-content text-center">
+                        <h3 class="overview-title">2008</h3>
+                        <p class="overview-text">17 năm kinh nghiệm</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-3">
+                <div class="overview-item">
+                    <div class="overview-icon">
+                        <figure>
+                            <img src="<?php bloginfo('template_url'); ?>/assets/images/Medal.png" alt="icon" loading="lazy">
+                        </figure>
+                    </div>
+                    <div class="overview-content text-center">
+                        <h3 class="overview-title">2008</h3>
+                        <p class="overview-text">17 năm kinh nghiệm</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
