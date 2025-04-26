24e86354263f13feaa046b2dc65ef32a4b45e2cb
newit-danhpv <danhpv@newit.co.jp>
2025-04-26 16:52:53 +0700

    commit blogs

---
+++ app/public/wp-content/themes/zekweb/loop_template/loop-item_highlights.php
@@ -0,0 +1,17 @@
+<div class="col-md-6 col-lg-3 loop-highlight">
+    <div class="loop-inner">
+        <figure class="loop-image">
+            <a href="">
+                <img src="https://images.unsplash.com/photo-1714578187196-29775454aa39?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGxhY2Vob2xkZXIlMjBpbWFnZXxlbnwwfHwwfHx8MA%3D%3D"
+                    alt="heading-icon">
+            </a>
+
+        </figure>
+
+        <div class="heading-wrapper">
+            <h4 class="loop-heading fs-18 text-black">top 1 trong 40 doanh nghiệp toạ đàm tại vtv</h2>
+        </div>
+
+        <a class="btn-hightlight bora-1">xem thêm</a>
+    </div>
+</div>
---
+++ app/public/wp-content/themes/zekweb/loop_template/loop-item_whyus.php
@@ -0,0 +1,14 @@
+<div class="col-md-6 col-lg-4 loop-whyus">
+    <div class="loop-inner">
+    <figure class="loop-image">
+            <a href="">
+                <img src="https://images.unsplash.com/photo-1714578187196-29775454aa39?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGxhY2Vob2xkZXIlMjBpbWFnZXxlbnwwfHwwfHx8MA%3D%3D"
+                    alt="heading-icon">
+            </a>
+
+        </figure>
+        <div class="heading-wrapper">
+            <h4 class="loop-heading fs-24">chuẩn giáo trình đào tạo</h2>
+        </div>
+    </div>
+</div>
