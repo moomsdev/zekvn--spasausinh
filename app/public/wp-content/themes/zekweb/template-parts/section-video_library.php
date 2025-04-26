a01b989eead6b68923fe66bc1e463ffe910f0f1c
newit-duylcq <newit-duylcq@newit.co.jp>
2025-04-26 10:04:54 +0700

    code done section video library

---
+++ app/public/wp-content/themes/zekweb/template-parts/section-video_library.php
@@ -0,0 +1,69 @@
+<?php
+$title = get_field('title', 'option');
+$image = get_field('image', 'option');
+?>
+
+<section class="section-video-library">
+    <div class="heading-wrapper">
+        <h2 class="section-heading fs-36">Thư viện mẹ yêu cầu tìm</h2>
+        <figure class="icon-image">
+            <img src="<?php bloginfo('template_url'); ?>/assets/images/heading-icon.png" alt="heading-icon">
+        </figure>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 video-library-main">
+                <div class="video-library-main-video">
+                    <div class="video-thumbnail mb-3">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/DOK-vjVC9D0?si=yiLDjWNxxvfO_8I8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    </div>
+                    <div class="video-caption">
+                        <div class="video-date">25/04/2025</div>
+                        <a href="/" class="video-title">[FEEDBACK KEM DA BON BON] - SẠCH RÔM SẢY, HẾT MẨN NGỨA</a>
+                        <div class="video-description">
+                            98% hiệu quả các vấn đề về da bé. Không chứa Corticoid, dùng được thường xuyên. Dịu da, mát da, ngăn mất nước, cân bằng độ ẩm. Không bết rít, không hăng, không nồng. Không bị để lại màu, vệt trên da. Dùng được cho trẻ từ sơ sinh.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 video-library-list">
+                <div class="row video-library-list-item mb-5">
+                    <div class="col-12 col-md-6">
+                        <div class="video-thumbnail">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/DOK-vjVC9D0?si=yiLDjWNxxvfO_8I8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="video-caption">
+                            <div class="video-date">25/04/2025</div>
+                            <a href="/" class="video-title">[FEEDBACK KEM DA BON BON] - SẠCH RÔM SẢY, HẾT MẨN NGỨA</a>
+                            <div class="video-description">
+                                98% hiệu quả các vấn đề về da bé. Không chứa Corticoid, dùng được thường xuyên. Dịu da, mát da, ngăn mất nước, cân bằng độ ẩm. Không bết rít, không hăng, không nồng. Không bị để lại màu, vệt trên da. Dùng được cho trẻ từ sơ sinh.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row video-library-list-item">
+                    <div class="col-12 col-md-6">
+                        <div class="video-thumbnail">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/DOK-vjVC9D0?si=yiLDjWNxxvfO_8I8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="video-caption">
+                            <div class="video-date">25/04/2025</div>
+                            <a href="/" class="video-title">[FEEDBACK KEM DA BON BON] - SẠCH RÔM SẢY, HẾT MẨN NGỨA</a>
+                            <div class="video-description">
+                                98% hiệu quả các vấn đề về da bé. Không chứa Corticoid, dùng được thường xuyên. Dịu da, mát da, ngăn mất nước, cân bằng độ ẩm. Không bết rít, không hăng, không nồng. Không bị để lại màu, vệt trên da. Dùng được cho trẻ từ sơ sinh.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="video-library-list-more">
+                    <button class="btn btn-primary video-library-more-btn">Xem tất cả</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
