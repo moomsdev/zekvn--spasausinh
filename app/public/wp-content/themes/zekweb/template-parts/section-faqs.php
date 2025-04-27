3d303c448ec70d1d0fa06d630000cd280f6c8712
newit-danhpv <danhpv@newit.co.jp>
2025-04-27 10:36:20 +0700

    commit faqs

---
+++ app/public/wp-content/themes/zekweb/template-parts/section-faqs.php
@@ -0,0 +1,18 @@
+<div class="faq-card p-4">
+  <h5 class="mb-4 fw-bold text-primary">Một số câu hỏi thường gặp</h5>
+
+  <div class="faq-item mb-3">
+    <h6 class="faq-question fw-bold text-primary mb-1">Có thể massage vùng lưng, hông từ tháng thứ mấy?</h6>
+    <p class="faq-answer mb-0">Từ tháng thứ 4, thai phụ có thể bắt đầu “massage nhẹ nhàng” vùng lưng, hông. Tùy thuộc vào cơ địa của mỗi mẹ, cô chăm sóc của Home Care sẽ dùng một lực phù hợp đảm bảo an toàn cho mẹ và thai nhi.</p>
+  </div>
+
+  <div class="faq-item mb-3">
+    <h6 class="faq-question fw-bold text-primary mb-1">Tôi có thể chăm sóc bà bầu tại nhà hay đến Spa?</h6>
+    <p class="faq-answer mb-0">Tùy vào hoàn cảnh thực tế của mẹ, đội ngũ nhân viên Home Care có thể hỗ trợ mẹ tại nhà. Hoặc mẹ có thể chủ động sắp xếp thời gian và lên lịch hẹn với Home Care để được chăm sóc sức khỏe tốt nhất.</p>
+  </div>
+
+  <div class="faq-item">
+    <h6 class="faq-question fw-bold text-primary mb-1">Mẹ ở xa có phát sinh phụ phí không?</h6>
+    <p class="faq-answer mb-0">Home Care phục vụ khách hàng khắp cả nước: Hà Nội, Đà Nẵng, Hồ Chí Minh, Quảng Ninh, Hải Phòng, Hải Dương, Bắc Ninh, Thái Bình,… địa chỉ nhà khách hàng cách trụ sở trong trong bán kính 10km sẽ không phát sinh thêm phụ phí đi lại. Xa hơn sẽ có phụ phí riêng tùy từng địa điểm.<br>Quý khách có nhu cầu chăm sóc bầu tại nhà, vui lòng liên hệ Home Care để được phục vụ chu đáo nhất.</p>
+  </div>
+</div>
