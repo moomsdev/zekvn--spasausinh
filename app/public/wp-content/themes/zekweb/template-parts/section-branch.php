ba04b6f7bc4c556c45ba30e0638301044c844d24
newit-duylcq <newit-duylcq@newit.co.jp>
2025-04-27 14:28:47 +0700

    code done section-branch

---
+++ app/public/wp-content/themes/zekweb/template-parts/section-branch.php
@@ -0,0 +1,17 @@
+<?php
+$title = get_field('title', 'option');
+$image = get_field('image', 'option');
+?>
+
+<section class="section-branch">
+    <div class="heading-wrapper">
+        <h2 class="section-heading fs-48">Home care ghi đậm dấu ấn trên dải chữ s:</h2>
+        <figure class="icon-image">
+            <img src="<?php bloginfo('template_url'); ?>/assets/images/heading-icon.png" alt="heading-icon">
+        </figure>
+    <div class="container">
+        <figure>
+            <img src="<?php bloginfo('template_url'); ?>/assets/images/map-viet-nam.png" alt="map-viet-nam">
+        </figure>
+    </div>
+</section>
