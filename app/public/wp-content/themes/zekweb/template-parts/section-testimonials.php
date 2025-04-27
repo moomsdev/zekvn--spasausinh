7ed0ab017923c7c9ad7bf7eaffcb470c20821d48
newit-duylcq <newit-duylcq@newit.co.jp>
2025-04-27 08:54:23 +0700

    code layout not slide section testimonials

---
+++ app/public/wp-content/themes/zekweb/template-parts/section-testimonials.php
@@ -0,0 +1,24 @@
+<section class="section-testimonials" style="background-image: url(<?php bloginfo('template_url'); ?>/assets/images/testimonials-bg.png); background-repeat: no-repeat; background-size: cover;">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-12 col-md-5 testimonials-video">
+                <iframe src="https://www.youtube.com/embed/ItRExComFJ4?si=gJfbFf52rKwQqYUy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+            <div class="col-12 col-md-7 testimonials-content">
+                <h3 class="testimonials-title mb-4">ĐÁNH GIÁ CỦA KHÁCH HÀNG</h3>
+                <div class="testimonials-description">
+                    <p>Dịch vụ sản phẩm bên cty rất ok, nhân viên làm chăm sóc rất tận tâm, nói chuyện rất dễ thương và gần gửi, mình rất thích dịch vụ bên cty.Cảm ơn rất nhiều vì đã giúp cho mẹ và bé đã hồi phục sức khỏe, xin cảm ơn rất nhiều.</p>
+                </div>
+                <figure class="testimonials-review">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/images/review-star.png" alt="review star Image">
+                </figure>
+                <div class="testimonials-profile">
+                    <figure>
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/testimonials-avatar.png" alt="testimonials avatar">
+                    </figure>
+                    <h3>THANH LÊ</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
