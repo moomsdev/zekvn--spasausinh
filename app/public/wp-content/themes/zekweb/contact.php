ecf6fe97e7c96440b8604e1f0c17089627842347
newit-duylcq <newit-duylcq@newit.co.jp>
2025-04-27 23:58:32 +0700

    code done contact page

---
+++ app/public/wp-content/themes/zekweb/contact.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Liên hệ
+ */
+?>
+<?php get_header(); ?>
+
+<main id="main">
+    <?php get_template_part('template-parts/section', 'contact'); ?>
+    <div class="container-fluid contact-container">
+        <div class="google-map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.904611732553!2d105.81388241542348!3d21.03650239288885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab1946cc7e23%3A0x87ab3917166a0cd5!2zQ8O0bmcgdHkgY-G7lSBwaOG6p24gY8O0bmcgbmdo4buHIFNBUE8!5e0!3m2!1svi!2s!4v1555900531747!5m2!1svi!2s" width="600" height="450" style="border:0" allowfullscreen=""></iframe>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
