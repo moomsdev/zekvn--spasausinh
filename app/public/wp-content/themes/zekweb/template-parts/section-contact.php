ae5abc824e1981e583d9fe63d2e156a4539c7f25
newit-danhpv <danhpv@newit.co.jp>
2025-04-29 09:23:43 +0700

    contact

--- app/public/wp-content/themes/zekweb/template-parts/section-contact.php
+++ app/public/wp-content/themes/zekweb/template-parts/section-contact.php
@@ -5,14 +5,19 @@ $image = get_field('image', 'option');
 
 <section class="section-contact">
     <div class="container">
-        <h2 class="section-heading"><?php echo $title; ?></h2>
-        <div class="row">
-            <div class="col-12 col-lg-4">
+        <div class="heading-wrapper">
+            <h2 class="section-heading fs-48">dấu ấn home care</h2>
+            <figure class="icon-image">
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/heading-icon.png" alt="heading-icon">
+            </figure>
+        </div>
+        <div class="row d-flex align-items-stretch">
+            <div class="col-md-12 col-lg-4">
                 <figure class="contact-image">
-                <img src="<?php bloginfo('template_url'); ?>/assets/images/sidebar-img-form.png" alt="heading-icon">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/images/sidebar-img-form.png" alt="heading-icon">
                 </figure>
             </div>
-            <div class="col-12 col-lg-8">
+            <div class="col-md-12 col-lg-8">
                 <div class="contact-form">
                     <?php echo do_shortcode('[contact-form-7 id="d4e589b" title="Liên hệ"]'); ?>
                 </div>
